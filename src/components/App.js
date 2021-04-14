0f0d0fb1cdfa4a79389f1527320802cd68b86d8e
Mate Marko <markomate90@gmail.com>
2021-04-14 17:52:15 +0200

    added feed

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Navbar from "./Navbar.js"
+import Navbar from "./Navbar.js";
 import { Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import history from '../History';
-import Generator from "./Generator"
+import Generator from "./Generator";
+import Feed from "./Feed";
+
 function App() {
 
   return ( 
@@ -17,6 +19,7 @@ function App() {
                     <Route path="/Login" component={Login} />
                     <Route path="/Register" component={Register} />
                     <Route path="/Generator" component={Generator} />
+                    <Route path="/Feed" component={Feed} />
                 </Switch>
     </div> 
     </div>
